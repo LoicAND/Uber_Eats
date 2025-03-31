be3f698d57f353fde4ddff70d1fb8ef6a4b777d9
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-01 00:22:10 +0200

    fix(RestaurantService): translate in English

---
+++ src/fr/ynov/ubereats/service/RestaurantService.java
@@ -0,0 +1,18 @@
+package fr.ynov.ubereats.service;
+
+import fr.ynov.ubereats.domain.restaurant.Restaurant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public void addRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
+
+    public List<Restaurant> listRestaurants() {
+        return new ArrayList<>(restaurants);
+    }
+}
