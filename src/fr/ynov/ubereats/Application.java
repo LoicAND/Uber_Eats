4a210044b313432e658c6031d800ed5df534f1ee
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-01 00:18:22 +0200

    fix(Application): translate in English

---
+++ src/fr/ynov/ubereats/Application.java
@@ -0,0 +1,10 @@
+import fr.ynov.ubereats.configuration.Configuration;
+import fr.ynov.ubereats.gui.GraphicalInterface;
+
+import javax.swing.*;
+
+public static void main(String[] args) {
+    Configuration config = new Configuration();
+    config.initialize();
+
+}
