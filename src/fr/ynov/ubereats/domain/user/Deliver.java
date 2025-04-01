c347057aefdaed7a29f210ea88b22888469b4f66
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-02 01:38:55 +0200

    fix(Deliver): fix some issues with names

--- src/fr/ynov/ubereats/domain/user/Deliver.java
+++ src/fr/ynov/ubereats/domain/user/Deliver.java
@@ -9,27 +9,24 @@ import java.util.List;
 public class Deliver extends User {
     private String vehicule;
     private DeliverStatus status;
-    private List<Order> deliver;
+    private List<Order> deliveries;
 
     public Deliver(String id, String nom, String email, String telephone, String vehicule) {
         super(id, nom, email, telephone, null, null);
         this.vehicule = vehicule;
         this.status = DeliverStatus.AVAILABLE;
-        this.deliver = new ArrayList<>();
+        this.deliveries = new ArrayList<>();
     }
 
-    public boolean accepteDeliver(Order order) {
+    public boolean accepteDelivery(Order order) {
         if (this.status == DeliverStatus.AVAILABLE) {
-            this.deliver.add(order);
+            this.deliveries.add(order);
             this.status = DeliverStatus.IN_DELIVERY;
             return true;
         }
         return false;
     }
 
-    public String getVehicule() {
-        return vehicule;
-    }
 
     public String getVehicleType() {
         return vehicule;
