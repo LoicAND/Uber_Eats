22976ab9b2a514b71908247c5c11fa2b5fd060c1
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-02 16:14:19 +0200

    fix(PaymentMethod): remove a string

--- src/fr/ynov/ubereats/domain/payment/PaymentMethod.java
+++ src/fr/ynov/ubereats/domain/payment/PaymentMethod.java
@@ -10,8 +10,4 @@ public enum PaymentMethod {
     PaymentMethod(String displayName) {
         this.displayName = displayName;
     }
-
-    public String getDisplayName() {
-        return displayName;
-    }
 }
