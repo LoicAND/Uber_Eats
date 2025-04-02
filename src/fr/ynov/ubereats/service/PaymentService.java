a4c07cb5d6ee4756b3c45f4a23bd56db09652708
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-02 16:13:00 +0200

    fix(PaymentService): add final and remove some functions

--- src/fr/ynov/ubereats/service/PaymentService.java
+++ src/fr/ynov/ubereats/service/PaymentService.java
@@ -7,41 +7,13 @@ import fr.ynov.ubereats.domain.payment.PaymentStatus;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 public class PaymentService {
-    private List<Payment> payments = new ArrayList<>();
+    private final List<Payment> payments = new ArrayList<>();
 
     public void savePayment(Payment payment) {
         payments.add(payment);
-    }
-
-    public Optional<Payment> findPayment(String id) {
-        for (Payment payment : payments) {
-            if (payment.getId().equals(id)) {
-                return Optional.of(payment);
-            }
-        }
-        return Optional.empty();
-    }
-
-    public Optional<Payment> findPaymentForOrder(Order order) {
-        for (Payment payment : payments) {
-            if (payment.getOrder().equals(order)) {
-                return Optional.of(payment);
-            }
-        }
-        return Optional.empty();
-    }
-
-    public List<Payment> listPaymentsByMethod(PaymentMethod method) {
-        List<Payment> paymentsByMethod = new ArrayList<>();
-        for (Payment payment : payments) {
-            if (payment.getMethod() == method) {
-                paymentsByMethod.add(payment);
-            }
-        }
-        return paymentsByMethod;
     }
 
     public Payment createPayment(Order order, PaymentMethod method) {
@@ -71,17 +43,4 @@ public class PaymentService {
         }
     }
 
-    public List<Payment> listPaymentsByStatus(PaymentStatus status) {
-        List<Payment> paymentsByStatus = new ArrayList<>();
-        for (Payment payment : payments) {
-            if (payment.getStatus() == status) {
-                paymentsByStatus.add(payment);
-            }
-        }
-        return paymentsByStatus;
-    }
-
-    public void deletePayment(String id) {
-        payments.removeIf(payment -> payment.getId().equals(id));
-    }
 }
