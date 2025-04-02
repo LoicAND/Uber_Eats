fbbc6981b4076edcd070051075130848150ecc40
Loic ANDRIANARIVONY <loic.andrianarivony@ynov.com>
2025-04-02 15:18:15 +0200

    feat(PaymentMethod): add a new method of payment

--- src/fr/ynov/ubereats/domain/payment/PaymentMethod.java
+++ src/fr/ynov/ubereats/domain/payment/PaymentMethod.java
@@ -1,6 +1,17 @@
 package fr.ynov.ubereats.domain.payment;
 
 public enum PaymentMethod {
-    CREDIT_CARD,
-    PAYPAL,
+    CREDIT_CARD("Carte bancaire"),
+    PAYPAL("Paypal"),
+    TICKET("Ticket restaurant");
+
+    private final String displayName;
+
+    PaymentMethod(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
